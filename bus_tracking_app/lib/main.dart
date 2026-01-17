594a326334a94174785a948a1d48f9c1879195cd
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2026-01-17 11:50:11 +0530

    Update main.dart

--- bus_tracking_app/lib/main.dart
+++ bus_tracking_app/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:bus_tracking_app/features/auth/login_screen.dart';
 import 'package:bus_tracking_app/routes/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.web,
+  );
+  
   runApp(const MyApp());
 }
 
